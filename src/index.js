37d54bf6555d2ce718878b11be13520c1e7027a3
Joaquín Bernal <jobedom@gmail.com>
2018-09-10 17:48:18 +0200

    Remove semicolons

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
 function deepFreeze(obj) {
-   const propNames = Object.getOwnPropertyNames(obj);
+   const propNames = Object.getOwnPropertyNames(obj)
    for (const name in propNames) {
-      const prop = obj[name];
+      const prop = obj[name]
       if (typeof prop === 'object' && prop !== null) {
-         deepFreeze(prop);
+         deepFreeze(prop)
       }
    }
-   return Object.freeze(obj);
+   return Object.freeze(obj)
 }
 
 function error(message) {
-   console.error(message);
+   console.error(message)
 }
 
 export default {
    install(Vue) {
       Vue.mixin({
          beforeCreate() {
-            const constants = this.$options.const;
-            if (!constants) return;
-            this.$options.computed = this.$options.computed || {};
+            const constants = this.$options.const
+            if (!constants) return
+            this.$options.computed = this.$options.computed || {}
             for (const key in constants) {
                if (key !== key.toUpperCase()) {
-                  error(`Constant '${key}' must be all upper case`);
-                  return;
+                  error(`Constant '${key}' must be all upper case`)
+                  return
                }
                if (constants.hasOwnProperty(key)) {
-                  const frozen = deepFreeze(constants[key]);
+                  const frozen = deepFreeze(constants[key])
                   this.$options.computed[key] = {
                      get: () => frozen,
                      set: () => error(`Constant '${key}' cannot be changed`),
-                  };
+                  }
                }
             }
          },
-      });
+      })
    },
-};
+}
